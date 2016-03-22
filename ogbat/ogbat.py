37c418baa95820a1b06ad87585ea6d0ac3a80013
Edmene <cristianedsonm@gmail.com>
2016-03-21 21:30:11 -0300

    Update ogbat.py

--- ogbat/ogbat.py
+++ ogbat/ogbat.py
@@ -7,7 +7,9 @@ parser.add_argument("option", help="Select what the tool should do (benchmark[b]
 args = parser.parse_args()
 if(args.option == "s"):
     system = system_info.SystemInformations()
-    print (system.cpu)
+    print ("CPU: "+system.cpu+"Kernel: "+system.kernelV+"\nMemory: "+system.memory+
+           "\nGPU: "+system.gpu[0]+"\nGPU Driver: "+system.gpu[1]+"\nGPU Driver Type: "+system.gpu[2]+
+           "\nGPU Memory: "+system.gpu[3]+"\nDesktop: "+system.desktopEnv+"\nResolution:"+system.resolution)
     
 elif(args.option == "b"):
     benchmark = benchmark.Benchmark._launch_game("")
